d1ec292736acf43e05b19048bdbd189eb0beff39
Corazon <41511738+assafgold1@users.noreply.github.com>
2023-11-21 22:56:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+const dir = 'api';
+const app = express();
+const port = 3000;
+const apiUrl = 'https://www.oref.org.il/WarningMessages/History/AlertsHistory.json';
 
+app.get('/api', async (req, res) => {  
+  try {
+    const response = await axios.get(apiUrl, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error making API request:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}/${dir}`);
+});
